619a8cb4f72244e02ebf5f49b436546128710be4
Garret Wilson <60952801+gwilson93@users.noreply.github.com>
2023-10-20 16:21:16 -0400

    colorCells function created for hover effect to generate color on a cell in grid

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-// DOM Manipulation
+// Initial Grid Creation
 const container = document.querySelector(".game-board");
+
+createGrid(16);
+colorCells("black");
 
 function createGrid (size) {
   boxes = size ** 2;
   for (let i = 1; i <= boxes; i++) {
     const cell = document.createElement("div");
-    cell.classList.add("cell");
+    cell.classList.add("cell","reset");
+    cell.style.minHeight = 500 / size + "px";
+    cell.style.minWidth = 500 / size + "px";
     container.appendChild(cell);
   }
 }
 
-createGrid(16);
+function colorCells (color) {
+  const allCells = document.querySelectorAll(".cell");
+  allCells.forEach((cell) => {
+    cell.addEventListener("mouseover",() =>{
+      cell.style.background = color;
+    })
+  })
+}
