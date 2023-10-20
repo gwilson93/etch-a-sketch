bf3ef2e494f5c15b27a8800afa55e3aa177d1bba
Garret Wilson <60952801+gwilson93@users.noreply.github.com>
2023-10-20 11:57:44 -0400

    createGrid function to generate grid based on size

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // DOM Manipulation
-const container = document.querySelector(".container");
+const container = document.querySelector(".game-board");
 
 function createGrid (size) {
-  
+  boxes = size ** 2;
+  for (let i = 1; i <= boxes; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+  }
 }
+
+createGrid(16);
